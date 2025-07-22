3290f9c94b10692fb1302a731e05d24d23d7522b
naprihmer <naprihmer9@yandex.ru>
2025-07-22 17:52:10 +0500

    marks tasks

--- fixtures/test_fixture8.py
+++ fixtures/test_fixture8.py
@@ -16,12 +16,15 @@ def browser():
 
 class TestMainPage1():
 
-    @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+    
+    @pytest.mark.xfail(reason='fixing now')
+    def test_guest_should_see_search_button_on_the_main_page(self, browser): 
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "input.btn.btn-default")
